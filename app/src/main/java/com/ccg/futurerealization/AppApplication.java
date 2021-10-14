11601a007bd277873bc91084c51e0a3234b7b8c4
CCG <1107079145@qq.com>
2021-10-14 22:29:13 +0800

    add litepal database

--- app/src/main/java/com/ccg/futurerealization/AppApplication.java
+++ app/src/main/java/com/ccg/futurerealization/AppApplication.java
@@ -1,10 +1,19 @@
 package com.ccg.futurerealization;
 
-import android.app.Application;
+import org.litepal.LitePal;
+import org.litepal.LitePalApplication;
 
 /**
  * @author：cgaopeng on 2021/10/14 21:41
  */
-public class AppApplication extends Application {
+public class AppApplication extends LitePalApplication {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //不继承LitePalApplication的话则使用这个
+        //LitePal.initialize(this);
+        //任意地方调一次此方法即创建数据库
+        LitePal.getDatabase();
+    }
 }
